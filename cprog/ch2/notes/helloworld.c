6972d9de004c0c3a5a38dba3534afa76bd879f3b
Adarsh Ujjual <ujjualadarsh@gmail.com>
2025-02-27 00:55:16 +0530

    Add helloworld with explanation [ch2]

---
+++ cprog/ch2/notes/helloworld.c
@@ -0,0 +1,9 @@
+//include is a directive
+//stdio.h is the standard input output header file
+#include <stdio.h> 
+                   
+
+int main() {
+    // printf is used to print anything on the screen so that it can be see by us
+    printf("Hello World"); 
+}
