d70363783c5b59ce9214cea3f500791490147a06
Adarsh Ujjual <ujjualadarsh@gmail.com>
2025-03-04 23:59:25 +0530

    edit practice5_3 in chapter 3 practice

--- cprog/ch3/notes/practice5_3.c
+++ cprog/ch3/notes/practice5_3.c
@@ -20,36 +20,38 @@ congratulate them and tell them how many attempts it took. If they run out of at
         scanf("%d",&guess);
 
         
-        while(atm_left>0){
+        while(atm_left>0){ // run only till attempts are left over
 
-            atm_left--;
-            atm_over++;
+            atm_left--;   // reduce attempts remaining by one after every unsuccessful attempt
+            atm_over++;   // increase attemps over by one after every unsuccessful attempt
         
-                if(guess<42){
+                if(guess<42){  // if less that secret num then print too low
                     printf("Too low!\n");
                    
                 }
                 
-                else if(guess>42){
+                else if(guess>42){  //if more that secret num then print too high
                     printf("Too high!\n");
                 }
                 
-                else if(guess==42){  // could use only else to improve readability
-                   
+                else if(guess==42){  // could use only else to improve readability 
+                                     // can print congrats upon guessing right 
                     printf("Congratulations you guessed the number 42 in %d attempts.\n",atm_over);
                 }
                 
-                if(atm_left>0 && guess != 42 ){
+                if(atm_left>0 && guess != 42 ){  //for every wrong attempt print the remaining attempts
                       printf("You have %d attempts left.\n",atm_left);
                 }
               
-                break;
+                break;// using the break statement to break to exit the while loop after one cycle. 
+                      // this ensures the while loop updates attempts exactly once per guess as only one guess
+                      // needs to processed at avery iteration.
 
         }
 
-    }while(atm_left>0 && guess!=42);
+    }while(atm_left>0 && guess!=42);  //looping condition that keeps game going; attemps left greater than one and guess is not right 
 
-        if(atm_left==0){
+        if(atm_left==0){   // all 5 attempt are over then print game over ...loop exit then msg will print 
             printf("Game over! The secret number was 42.\n");
         }
 
